643336284a61e4a7babca573df63ed1b77a05d68
Nishant260220 <tyaginishu015@gmail.com>
2025-11-22 11:28:26 +0530

    add layout, module, sidebar, user button, and global style updates

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,22 @@
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { DashboardSidebar } from "@/modules/dashboard/ui/components/dashboard-sidebar";
+import React from "react";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children}: Props) => {
+   return (
+    <div>
+      <SidebarProvider>
+        <DashboardSidebar/>
+        <main className="flex flex-col h-screen w-screen bg-muted">
+           {children}
+        </main>
+      </SidebarProvider>
+    </div>
+   )
+}
+
+export default Layout;
