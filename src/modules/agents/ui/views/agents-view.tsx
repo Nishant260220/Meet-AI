3a7a30ec2020471b574ae2eabb212cd702d88fde
Nishant260220 <tyaginishu015@gmail.com>
2025-11-26 15:36:27 +0530

    add responsive dialog and responsive command dialog

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -5,7 +5,7 @@ import { LoaderState } from "@/components/loader-state";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
 
-export const AgentView = () => {
+export const    AgentView = () => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(
     trpc.agents.getMany.queryOptions()
