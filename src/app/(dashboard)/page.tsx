3ebaa789fbd114fd1d9f3a05eacd726648bde322
Nishant260220 <tyaginishu015@gmail.com>
2025-11-26 21:13:14 +0530

    add protectedProcedure, agent create procedure, ListHeader, new agent dialog & reusable AgentForm

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -3,15 +3,15 @@ import { HomeView } from "@/modules/home/ui/views/home-view";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
-const Page = async() => {
+const Page = async () => {
   const session = await auth.api.getSession({
     headers: await headers(),
-  })
+  });
 
-  if(!session) {
+  if (!session) {
     redirect("/sign-in");
   }
-  return <HomeView/>
-}
+  return <HomeView />;
+};
 
 export default Page;
--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,10 +1,24 @@
 import { db } from "@/db";
 import { agents } from "@/db/schema";
-import { createTRPCRouter, baseProcedure } from "@/trpc/init";
-import { TRPCError } from "@trpc/server";
+import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { agentsInsertSchema } from "../schema";
+import z from "zod";
+import { eq } from "drizzle-orm";
+// import { TRPCError } from "@trpc/server";
 
 export const agentsRouter = createTRPCRouter({
-  getMany: baseProcedure.query(async () => {
+  // TODO: Change 'getOne' to use 'Protectedprocedure'
+  getOne: protectedProcedure.input(z.object({ id: z.string()})).query(async ({ input }) => {
+    const [existingAgent] = await db
+    .select()
+    .from(agents)
+    .where(eq(agents.id, input.id))
+
+    return existingAgent;
+  }),
+
+  // TODO: Change 'getMany' to use 'Protectedprocedure'
+  getMany: protectedProcedure.query(async () => {
     const data = await db
         .select()
         .from(agents);
@@ -13,4 +27,18 @@ export const agentsRouter = createTRPCRouter({
         // throw new TRPCError({ code: "BAD_REQUEST"});
         return data
   }),
+
+  create: protectedProcedure
+  .input(agentsInsertSchema)
+  .mutation(async ({ input, ctx}) => {
+    const [createdAgents] = await db
+      .insert(agents)
+      .values({
+        ...input,
+        userId: ctx.auth.user.id,
+      })
+      .returning();
+
+      return createdAgents;
+  })
 })
