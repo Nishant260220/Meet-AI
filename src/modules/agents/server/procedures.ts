bb853cf94e395f7613667a3c0ccc9908407bd647
Nishant260220 <tyaginishu015@gmail.com>
2025-11-27 17:47:16 +0530

    Add a data table component and empty component with empty image

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -3,14 +3,17 @@ import { agents } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { agentsInsertSchema } from "../schema";
 import z from "zod";
-import { eq } from "drizzle-orm";
+import { eq, getTableColumns, sql } from "drizzle-orm";
 // import { TRPCError } from "@trpc/server";
 
 export const agentsRouter = createTRPCRouter({
   // TODO: Change 'getOne' to use 'Protectedprocedure'
   getOne: protectedProcedure.input(z.object({ id: z.string()})).query(async ({ input }) => {
     const [existingAgent] = await db
-    .select()
+    .select({
+      meetingCount: sql<number>`5`,
+      ...getTableColumns(agents)
+    })
     .from(agents)
     .where(eq(agents.id, input.id))
 
