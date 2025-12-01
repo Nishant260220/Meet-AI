b10c7d68b34a547b19bd846ffc1f0b3127c5a6d0
Nishant260220 <tyaginishu015@gmail.com>
2025-12-01 17:21:42 +0530

    Modify 'agents.getOne' procedure, add agent Id page, add data table redirect on click, add agent Id view

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -9,9 +9,11 @@ import { columns } from "../components/columns";
 import { EmptyState } from "@/components/empty-state";
 import { useAgentsFilters } from "../../hooks/use-agents-filters";
 import { DataPagination } from "../components/data-pagination";
+import { useRouter } from "next/navigation";
 
 
 export const AgentView = () => {
+  const router = useRouter();
   const [filters, setFilters] = useAgentsFilters();
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions({
@@ -20,8 +22,12 @@ export const AgentView = () => {
 
   return (
     <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
-      <DataTable data={data.items} columns={columns} />
-      <DataPagination 
+      <DataTable
+       data={data.items}
+        columns={columns}
+        onRowClick={(row) => router.push(`/agents/${row.id}`)}
+         />
+      <DataPagination
         page={filters.page}
         totalPages={data.totalPages}
         onPageChange={(page) => setFilters({ page })}
